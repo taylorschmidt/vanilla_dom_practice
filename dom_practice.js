715efc96b68b82324e7e003b6f0968e903ab1922
taylorschmidt <taylorhillschmidt@gmail.com>
2020-09-06 10:53:08 -0600

    queryactivity 1 query and change some elements

---
+++ dom_practice.js
@@ -0,0 +1,13 @@
+const restaurantSection = document.getElementById('restaurants');
+console.log(restaurantSection)
+
+const h2 = document.querySelector('h2')
+console.log(h2)
+
+const requiredElement = document.getElementsByClassName('required')
+console.log(requiredElement)
+
+
+const lis = document.querySelectorAll('li')
+lis[0].style.color = 'blue'
+lis[2].innerText = 'Jamboree sandwich at the Jambo-ree'
